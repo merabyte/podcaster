9c4125c4521c0d9bf2438e4bb57387d96c5e7bbf
raul.mera <raul.mera@domestika.org>
2023-03-24 15:43:54 +0100

    Episode page

--- src/pages/Episode/index.jsx
+++ src/pages/Episode/index.jsx
@@ -1,5 +1,28 @@
+import { useParams } from 'react-router-dom'
+
+import { usePodcastDetailRequest } from '../../hooks'
+import PodcastLayout from '../../components/PodcastLayout'
+
 function Episode() {
-  return <div className="text-red-700 font-bold">EPISODE</div>
+  const { podcastId, episodeId } = useParams()
+  const { data, loading } = usePodcastDetailRequest({ podcastId })
+  const episode = data?.episodes.find(item => `${item.id}` === episodeId) || {}
+
+  return (
+    <PodcastLayout data={data} loading={loading}>
+      <div className="flex flex-col border-solid border border-gray-300 shadow-md p-4 text-sm">
+        <div className="pb-4 border-b border-gray-300">
+          <div className="text-lg font-bold mb-2">{episode.title}</div>
+          <div className="text-sm" dangerouslySetInnerHTML={{ __html: episode.description }} />
+        </div>
+        <div className="flex pt-4">
+          <audio controls className="flex-1">
+            <source src={episode.audioUrl} type="audio/mpeg" />
+          </audio>
+        </div>
+      </div>
+    </PodcastLayout>
+  )
 }
 
 export default Episode
