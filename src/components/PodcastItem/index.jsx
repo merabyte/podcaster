289546195422a00fd92ad7592220877307d27dd4
raul.mera <raul.mera@domestika.org>
2023-03-24 15:41:16 +0100

    Home page

---
+++ src/components/PodcastItem/index.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+
+function PodcastItem({ title, author, imageUrl, href, boxStyle }) {
+  return (
+    <Link to={href} className={'group relative pt-7 flex flex-col'}>
+      <img
+        src={imageUrl}
+        loading="lazy"
+        alt={`${title} (${author})`}
+        className="absolute top-0 self-center rounded-full border-solid border border-gray-300 w-14 transition-all group-hover:scale-150"
+      />
+
+      <div
+        style={boxStyle}
+        className="flex-1 flex flex-col items-center pt-10 gap-y-2 p-2 border-solid border border-gray-300 shadow-md"
+      >
+        <div className="text-xs text-center uppercase">{title}</div>
+        <div className="text-xs text-center text-gray-500">Author: {author}</div>
+      </div>
+    </Link>
+  )
+}
+
+export default PodcastItem
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,85 @@
+import { useEffect, useState } from 'react'
+
+import { usePodcastListRequest } from '../../hooks'
+import PodcastItem from '../../components/PodcastItem'
+import Loading from '../../components/Loading'
+
+const COLORS = [
+  'rgb(204, 251, 241)',
+  'rgb(186, 230, 253)',
+  'rgb(229, 231, 235)',
+  'rgb(165, 243, 252)',
+  'rgb(186, 230, 253)',
+  'rgb(191, 219, 254)',
+  'rgb(199, 210, 254)',
+]
+
 function Home() {
-  return <div className="text-red-700 font-bold">HOME</div>
+  const { data, loading } = usePodcastListRequest()
+  const [filteredResults, setFilteredResults] = useState([])
+  const [searchText, setSearchText] = useState('')
+
+  useEffect(() => {
+    const dataList = data || []
+    const searchRx = new RegExp(searchText, 'i')
+    const newResults = searchText
+      ? dataList.filter(podcast => {
+          const { title, author } = podcast
+          return searchRx.test(title + author)
+        })
+      : dataList
+
+    setFilteredResults(newResults)
+  }, [data, searchText])
+
+  return (
+    <div className="flex flex-col gap-y-5">
+      <div className="flex items-center justify-end gap-x-3">
+        <span className="w-10 p-0.5 text-center bg-blue-700 font-semibold text-white rounded-md">
+          {filteredResults.length}
+        </span>
+        <input
+          type="text"
+          placeholder="Filter podcasts..."
+          className="w-56 border py-0.5 px-1 font-light"
+          maxLength={25}
+          disabled={loading}
+          value={searchText}
+          onChange={ev => setSearchText(ev.target.value)}
+        />
+      </div>
+
+      {loading && <Loading className="self-center" />}
+
+      {!loading && !filteredResults?.length && (
+        <div className="pt-8 self-center text-center italic text-gray-600">
+          <p>No results found.</p>
+          <p>Please, try again with a different text query.</p>
+        </div>
+      )}
+
+      {!loading && filteredResults && (
+        <div className="grid grid-cols-4 gap-x-5 gap-y-14">
+          {filteredResults.map((podcast, i) => {
+            const href = `/podcast/${podcast.id}`
+            const colorIndex = i % COLORS.length
+            const color = COLORS[colorIndex]
+
+            return (
+              <PodcastItem
+                key={podcast.id}
+                href={href}
+                boxStyle={{
+                  backgroundImage: `linear-gradient(45deg, ${color}, transparent)`,
+                }}
+                {...podcast}
+              />
+            )
+          })}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Home
